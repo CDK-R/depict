04472bbc0cdac262eee8c0a2bc421aec74d4c447
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-01-09 20:59:34 -0500

    consolidate IO into IO.R

--- R/IO.R
+++ R/IO.R
@@ -28,3 +28,23 @@ parse_smiles <- function(smi, generatecoords=TRUE) {
 
 }
 
+#' read_mol
+#'
+#' Read a Molfile and return an AtomContainer Object
+#'
+#' @param molfile Required. A filepath to a MOLfile.
+#' @return an AtomContainer
+#' @export
+read_mol <- function(molfile) {
+  jfile          <- J("java.io.File")
+  jfreader       <- J("java.io.FileReader")
+  jAtomContainer <- J("org.openscience.cdk.AtomContainer")
+  jMDLreader     <- J("org.openscience.cdk.io.MDLV2000Reader")
+
+  jfileobj   <- new(jfile, molfile)
+  jreaderobj <- new(jfreader, jfileobj)
+  jmolreader <- new(jMDLreader, jreaderobj)
+
+  jmolreader$read(new(jAtomContainer))
+}
+
